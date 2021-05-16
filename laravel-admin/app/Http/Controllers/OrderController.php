766e7d9aa0850ed381918e87af348b3360f5a8c4
tomafoks <tomafoks@mail.ru>
2021-05-17 02:24:36 +0500

    added Export CSV Files in OrderController

--- laravel-admin/app/Http/Controllers/OrderController.php
+++ laravel-admin/app/Http/Controllers/OrderController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
+use Response;
 
 class OrderController extends Controller
 {
@@ -17,5 +19,36 @@ class OrderController extends Controller
     function show($id)
     {
         return new OrderResource(Order::find($id));
+    }
+
+    function export()
+    {
+        $headers = [
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=orders.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" => "0",
+        ];
+
+        $callback = function () {
+            $orders = Order::all();
+            $file = fopen("php://output", 'w');
+
+            //header row
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            //body
+            foreach ($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, '', '', '']);
+                foreach ($order->orderItems() as $orderItem) {
+                    fputcsv($file, ['', '', '', $orderItem->product_title, $orderItem->price, $orderItem->quantity]);
+                }
+            };
+
+            fclose($file);
+        };
+
+        return Response::stream($callback, 200, $headers);
     }
 }
--- laravel-admin/app/Models/Order.php
+++ laravel-admin/app/Models/Order.php
@@ -41,4 +41,9 @@ class Order extends Model
             return $item->price * $item->quantity;
         });
     }
+
+    function getNameAttribute()
+    {
+        return $this->first_name . '.' . $this->last_name;
+    }
 }
