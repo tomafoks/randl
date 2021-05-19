8379cd67e69844a08f0511e65e99523cc2e67800
tomafoks <tomafoks@mail.ru>
2021-05-19 15:05:44 +0500

    added Permissions from users

--- laravel-admin/routes/api.php
+++ laravel-admin/routes/api.php
@@ -24,7 +24,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::put('users/info', 'UserController@updateInfo');
     Route::put('users/password', 'UserController@updatePassword');
     Route::post('upload', 'ImageController@upload');
-    Route::get('export', 'OrderController@export');
+    Route::get('export', 'OrderController@export'); //export SCV file
 
     Route::apiResource('users', 'UserController');
     Route::apiResource('roles', 'RoleController');
