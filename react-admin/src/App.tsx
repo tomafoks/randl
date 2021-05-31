dfa57e98e28dd443983b018ea9c007867d99c997
tomafoks <tomafoks@mail.ru>
2021-05-31 13:28:14 +0500

    add template

--- react-admin/src/App.tsx
+++ react-admin/src/App.tsx
@@ -4,22 +4,163 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+        <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search" />
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap">
+              <a className="nav-link" href="#">Sign out</a>
+            </li>
+          </ul>
       </header>
-    </div>
+
+        <div className="container-fluid">
+          <div className="row">
+            <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+              <div className="position-sticky pt-3">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      <span data-feather="home"></span>
+              Dashboard
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="file"></span>
+              Orders
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="shopping-cart"></span>
+              Products
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="users"></span>
+              Customers
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="bar-chart-2"></span>
+              Reports
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="layers"></span>
+              Integrations
+            </a>
+                  </li>
+                </ul>
+
+                <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                  <span>Saved reports</span>
+                  <a className="link-secondary" href="#" aria-label="Add a new report">
+                    <span data-feather="plus-circle"></span>
+                  </a>
+                </h6>
+                <ul className="nav flex-column mb-2">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="file-text"></span>
+              Current month
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="file-text"></span>
+              Last quarter
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="file-text"></span>
+              Social engagement
+            </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <span data-feather="file-text"></span>
+              Year-end sale
+            </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 className="h2">Dashboard</h1>
+                <div className="btn-toolbar mb-2 mb-md-0">
+                  <div className="btn-group me-2">
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Share</button>
+                    <button type="button" className="btn btn-sm btn-outline-secondary">Export</button>
+                  </div>
+                  <button type="button" className="btn btn-sm btn-outline-secondary dropdown-toggle">
+                    <span data-feather="calendar"></span>
+            This week
+          </button>
+                </div>
+              </div>
+
+              <canvas className="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+              <h2>Section title</h2>
+              <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                      <th>Header</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1,001</td>
+                      <td>random</td>
+                      <td>data</td>
+                      <td>placeholder</td>
+                      <td>text</td>
+                    </tr>
+                    <tr>
+                      <td>1,002</td>
+                      <td>placeholder</td>
+                      <td>irrelevant</td>
+                      <td>visual</td>
+                      <td>layout</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>data</td>
+                      <td>rich</td>
+                      <td>dashboard</td>
+                      <td>tabular</td>
+                    </tr>
+                    <tr>
+                      <td>1,003</td>
+                      <td>information</td>
+                      <td>placeholder</td>
+                      <td>illustrative</td>
+                      <td>data</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
   );
 }
 
